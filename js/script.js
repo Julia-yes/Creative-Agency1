a564ac61e3feb77837b33e2c9c9982f9403d6f3a
Julia-yes <ec.ho@mail.ru>
2022-06-14 17:19:58 +0400

    a half of mobile version

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".h-list");
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle("hamburger_close");
+    nav.classList.toggle("h-list_phone");
+})
