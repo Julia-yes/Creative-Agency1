7163aabacb1935ee5a1da06736c0dd58079a74a9
Julia-yes <ec.ho@mail.ru>
2022-06-17 17:50:08 +0400

    adaptive

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".h-list");
+const body = document.querySelector(".main");
 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle("hamburger_close");
     nav.classList.toggle("h-list_phone");
+    body.classList.toggle("modal-window");
 })
